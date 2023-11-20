e478c4cfb3c7c993fb057a595d3c5166e1cd867a
husnitdin <husnitdin@gmail.com>
2023-11-20 15:27:27 +0500

    init commit

--- src/main/java/uz/geeks/cab_book_user/service/LocationService.java
+++ src/main/java/uz/geeks/cab_book_user/service/LocationService.java
@@ -1,18 +1,12 @@
 package uz.geeks.cab_book_user.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationService {
-
-
-    @KafkaListener
-    public boolean cabLocation(String location) {
-
-        return true;
+    @KafkaListener(topics = "cab-location", groupId = "user-group")
+    public void cabLocation(String location) {
+        System.out.println(location);
     }
-
 }
